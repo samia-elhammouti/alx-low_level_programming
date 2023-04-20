c04fedd0daf3df5868a000a07fa5c9b8e868bdcf
samia-elhammouti <samiaelhammouti1@gmail.com>
2023-04-20 08:43:58 -0700

    TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-  * main - 4-puts.c 
-  * description - Write a C program that prints exactly 
+  * main - 4-puts.c
   *
   * Return: Always 0 (Success) 
   */
-
 int main(void)
 {
        	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+       	return (0);
 }
