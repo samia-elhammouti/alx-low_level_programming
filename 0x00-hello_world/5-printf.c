ba7dda9c0cf505fefe079c9743e8e6ebe9351c5a
samia-elhammouti <samiaelhammouti1@gmail.com>
2023-04-20 08:40:27 -0700

    TASK 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - 5-printf.c
- * description - Write a C program that prints exactly with proper grammar,
- *
- * Return: Always 0 (Success)
- */
-
+  * main - 5-printf.c
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
-{
-       	printf("with proper grammar, but the outcome is a piece of art,\n");
-       	return (0);
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
